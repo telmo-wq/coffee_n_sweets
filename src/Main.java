2037c91d516a373739ae29091f8c29ec00e5b205
Neivals <tnls@cesar.school>
2026-09-24 19:06:22 -0300

    Testes iniciais de conexão entre Java e MySQL

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class Main {
-
     public static void main(String[] args) {
         try (Connection conn = Conexao.getConexao()) {
             System.out.println("Conectado com sucesso!");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT COUNT(*) AS total FROM Cliente");
+
+            if (rs.next()) {
+                int total = rs.getInt("total");
+                System.out.println("Total de clientes no banco: " + total);
+            }
+
         } catch (SQLException e) {
             System.out.println("Falha na conexão: " + e.getMessage());
         }
