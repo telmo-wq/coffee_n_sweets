b75d1d4f83c539ac258e004a150d58e4787ec3f2
Ian Felipe <ian.felipe.drunen@gmail.com>
2026-09-25 11:17:09 -0300

    Adicionando sql das 4 consultas

---
+++ Scripts/consultas.sql
@@ -0,0 +1,19 @@
+use coffee_n_sweets;
+
+select Data, SUM(Valor_total), Forma_pagamento from pedido_pagamento group by Data, Forma_pagamento;
+
+select p.ID_produto, p.Nome, SUM(i.Quantidade) as Total_vendido from item_pedido i 
+join produto p on i.ID_produto = p.ID_produto group by p.ID_produto, p.Nome order by Total_vendido desc limit 10;
+
+select f.ID_funcionario, f.Nome, COUNT(distinct p.Numero_item, p.ID_pagamento) as Itens_preparados from prepara p
+join funcionario f on p.ID_funcionario = f.ID_funcionario where f.cargo = 'Barista' group by f.ID_funcionario, f.Nome having Itens_preparados > (
+	select AVG(Itens_preparados2) from (
+		select COUNT(distinct p2.Numero_item, p2.ID_pagamento) as Itens_preparados2 from prepara p2
+		join funcionario f2 on p2.ID_funcionario = f2.ID_funcionario where f2.cargo = 'Barista' group by f2.ID_funcionario
+	) as Media_preparo
+);
+
+select p.Nome, g.Nome, g.Origem, fo.Razao_social, fn.Preco_kg from cafe c join grao g on c.ID_grao = g.ID_grao join fornece fn on g.ID_grao = fn.ID_grao
+join fornecedor fo on fn.ID_fornecedor = fo.ID_fornecedor join produto p on c.ID_produto = p.ID_produto order by p.Nome;
+
+
